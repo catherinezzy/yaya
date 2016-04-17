f6dbd291a04aed775d2de021ddd49ed68a85b228
Ziyue <catherine000000@gmail.com>
2016-04-17 20:24:35 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yaya::Application.config.secret_key_base = '2a9bcdc552c0c47f16af84708b4370ff7db59bfb5ed17672f36001f91a526e045707e00754ec917e216e5a581cb5f8b15bc8bc1b04a2180c44b864d624b53317'
